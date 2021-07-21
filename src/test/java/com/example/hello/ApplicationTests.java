b6f320fe129c67bce60dab3fb55f27217d68f0b4
Jason0317 <34262552+Jason0317@users.noreply.github.com>
2021-07-21 23:19:26 +0800

    修改单元测试函数

--- src/test/java/com/example/hello/ApplicationTests.java
+++ src/test/java/com/example/hello/ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.hello;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,7 +8,7 @@ public class ApplicationTests {
 
     @Test
     public String contextLoads() {
-        return "hello world";
+        return "hello world ss";
     }
 
 }
